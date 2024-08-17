2fb405092cdd5401ac6a1319690c00136eccf581
fabienfrfr <fabien.furfaro@gmail.com>
2024-08-17 15:19:13 +0200

    embedding works!

--- mamba_bys/mambabys.py
+++ mamba_bys/mambabys.py
@@ -34,7 +34,7 @@ def Color_rquantization(img, max_size=20, nb_colors=216):
     # Convertir en image 8 bits
     return img_c.astype(np.uint8)
 
-def input_seq_construct(arr, dim=3):
+def input_seq_construct(arr, dim=3, sep_val=255):
     if dim % 2 == 0 : dim +=1
     pw = (dim-1)//2
     # Padding around array and size
@@ -50,12 +50,12 @@ def input_seq_construct(arr, dim=3):
     # if image data, including skip line
     if shape[0] > 1 :
         separator = np.zeros((shape[0], 1, dim, dim), dtype=np.uint8)
-        separator[:, 0, 1, 1] = 255
+        separator[:, 0, 1, 1] = sep_val
         result = np.concatenate((result, separator), axis=1)
         # Linearize
-        return result.reshape(-1, dim, dim)[:-1]
+        return result.reshape(-1, dim, dim)[:-1][None]
     else :
-        return result.reshape(-1, dim, dim)
+        return result.reshape(-1, dim, dim)[None]
 
 @dataclass
 class MambaConfig:
@@ -64,37 +64,37 @@ class MambaConfig:
     d_conv : int = 4 # The convolutionnal windows
     expand: int = 2 # E in paper/comments
     depth : int = 8 # The number of residual S6 layers
+    vocab_size : int = 256+216+2 # ASCII bytes + RGB 6*6*6 pixel + img step/stop
 
 class BysMamba(nn.Module):
     def __init__(self, config: MambaConfig):
         super().__init__()
         self.config = config
         # text & image(t) embedding
-        self.vocab_size = 256+216 # ASCII bytes + RGB 6*6*6 pixel
-        self.linear_embedding = nn.Embedding(self.vocab_size, config.dim)
-        self.patch_embedding = nn.Conv2d(in_channels=1, out_channels=self.vocab_size, kernel_size=3, stride=1, padding=0) # 3D in future
+        self.linear_embedding = nn.Embedding(config.vocab_size, config.dim)
+        self.patch_embedding = nn.Conv2d(in_channels=config.dim, out_channels=config.dim, kernel_size=3, stride=1, padding=0) # 3D in future
         # mamba part
         self.in_mamba = Mamba(d_model=config.dim, d_state=config.d_state, d_conv=config.d_conv, expand=config.expand,)
         self.layers = nn.ModuleList([Mamba(d_model=config.dim, d_state=config.d_state, d_conv=config.d_conv, expand=config.expand,) for _ in range(config.depth)])
         self.out_mamba = Mamba(d_model=config.dim, d_state=config.d_state, d_conv=config.d_conv, expand=config.expand,)
         # output
-        self.lm_head = nn.Linear(config.dim, self.vocab_size, bias=False)
+        self.lm_head = nn.Linear(config.dim, config.vocab_size, bias=False)
 
     def forward(self, x):
-        # shape : x : (B, L, M=3, N=3)
+        # shape : x : (B, L, M=3, N=3) : long
         B,L,M,N = x.shape
+        dim = self.config.dim
         # embedding
-        #xl = x[:, :, M//2, N//2] # img center
-        #xl = self.linear_embedding(xl) # (B,L,D)
-        xp = self.patch_embedding(input_tensor.view(B*N, 1, 3, 3)).view(B, L, self.vocab_size)
-        x = xp #+ xl
+        x = self.linear_embedding(x.view(B*L, M, N)).squeeze()
+        x = x.permute(0, 3, 1, 2)  # (batch_size*L, embedding_dim, height, width)
+        x = self.patch_embedding(x).view(B, L, dim) # (B,L,D)
         # bidirectional mamba input
-        x += self.in_mamba(x) + self.in_mamba(torch.flip(x, dims=[1])).flip([1])
+        x += (self.in_mamba(x) + self.in_mamba(torch.flip(x, dims=[1])).flip([1]))/2
         # mamba intermediate layers
         for layer in self.layers:
             x += layer(x)
         # bidirectional mamba output
-        x += self.out_mamba(x) + self.out_mamba(torch.flip(x, dims=[1])).flip([1])
+        x += (self.out_mamba(x) + self.out_mamba(torch.flip(x, dims=[1])).flip([1]))/2
         # prediction output
         x = self.lm_head(x) # probability
         return x
